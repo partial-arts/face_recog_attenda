edb649bb0b5b2de0b43320fca05cc1105c3a4a63
partial-arts <109165674+partial-arts@users.noreply.github.com>
2025-01-17 15:02:22 +0800

    Update face_recognize.py

--- face_recognize.py
+++ face_recognize.py
@@ -1,8 +1,8 @@
 import threading
-import cv2 
+import cv2
 from deepface import DeepFace
 
-cap = cv2.VideoCapture (0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -13,61 +13,55 @@ face_match = False
 
 reference_img = cv2.imread("reference.jpg")
 
+
 def check_face(frame):
-  global face_match
+    global face_match
 
-  try:
-    if DeepFace.verify (frame, reference_img.copy())['verified']:
-      face_match=True
-    else:
-      face_match=False
-  except ValueError: 
-    face_match=False
- 
+    try:
+        if DeepFace.verify(frame, reference_img.copy())['verified']:
+            face_match = True
+        else:
+            face_match = False
+    except ValueError:
+        face_match = False
+
+
 while True:
+    ret, frame = cap.read()
 
- ret, frame = cap.read()
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
 
- if ret:
-    if counter % 38 == 8:
+        if face_match:
+            cv2.putText(
+                frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3
+            )
 
-      try:
-       threading. Thread(target=check_face, args=(frame.copy(),)).start() 
-      except ValueError:
+        else:
+            cv2.putText(
+                frame,
+                "NO MATCH!",
+                (20, 450),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                2,
+                (0, 0, 255),
+                3,
+            )
 
-       pass
-    counter += 1
+        cv2.imshow("video", frame)
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
 
-    if face_match:
-
-      cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-
-    else:
-
-     cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
-
-    cv2.imshow("video", frame)
- key = cv2.waitKey(1)
- if key == ord("q"):
-    break
 cv2.destroyAllWindows()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+## 821 GB free of ....
 
 ## Source: https://www.youtube.com/watch?v=pQvkoaevVMk
 ## In the shubhamkumar27_face_recog_attendance/main.py, inspire the "replace" of the part of face recognition with this program
